5e9dc300b0858f7c6fe5338923d1de6c957794cb
kh3dron <tristansaldanha@gmail.com>
2023-11-04 10:01:17 -0400

    multi_indicator

--- utilities/add_indicators.py
+++ utilities/add_indicators.py
@@ -1,5 +1,14 @@
+import pandas as pd
+
+def add_indicators(df, fields):
+    if "RSI_14" in fields:
+        df = RSI(df, 14)
+    if "EMA_50" in fields:
+        df = EMA(df, 50)
+    return df
+
 def RSI(df, n):
-    
+    fieldName = "RSI_" + str(n)
     delta = df["close"].diff()
     delta = delta[1:]
     up, down = delta.copy(), delta.copy()
@@ -11,6 +20,11 @@ def RSI(df, n):
     AVG_Loss = abs(df["down"].rolling(window=n).mean())
     RS = AVG_Gain / AVG_Loss
     RSI = 100.0 - (100.0 / (1.0 + RS))
-    df["RSI_14"] = RSI.round(4)
+    df[fieldName] = RSI.round(4)
     return df
 
+def EMA(df, n):
+    fieldName = "EMA_" + str(n)
+    EMA = pd.Series(df['close'].ewm(span=n, min_periods=n).mean(), name=fieldName)
+    df = df.join(EMA)
+    return df
