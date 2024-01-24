f69b791695386fa3f0f3ff98eeb17dd7e2380a5d
kh3dron <tristansaldanha@gmail.com>
2024-01-23 17:24:28 -0800

    backtesting strategy abstraction

---
+++ backtest_engine.py
@@ -0,0 +1,36 @@
+class Backtest_Strategy():
+    # This class should contain all the logic for a trading strategy to conduct a backtest. 
+    # It will make assumptions about columns in the dataframe that is used with.
+
+    def __init__():
+        pass
+
+    def LongEnter(self):
+        pass
+
+    def LongProfit(self):
+        pass
+
+    def LongStopLoss(self):
+        pass
+
+    def LongTimeout(self):
+        pass
+
+    def ShortEnter(self):
+        pass
+
+    def ShortProfit(self):
+        pass
+
+    def ShortStopLoss(self):
+        pass
+
+    def ShortTimeout(self):
+        pass
+
+    def Backtest(df):
+        # This function accepts a df of historic stock data and an indicator logic object.
+        # It will return a new dataframe of times when the indicator logic enters and exits long and short positions. 
+        # cols = ['Ticker', 'Date', 'Time', 'Action']
+        pass
