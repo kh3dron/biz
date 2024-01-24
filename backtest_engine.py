2a942fd814446a971c450940bc6ec10c97797725
kh3dron <tristansaldanha@gmail.com>
2024-01-24 14:44:54 -0800

    more setup

--- backtest_engine.py
+++ backtest_engine.py
@@ -1,36 +1,74 @@
+import pandas as pd
+
 class Backtest_Strategy():
     # This class should contain all the logic for a trading strategy to conduct a backtest. 
     # It will make assumptions about columns in the dataframe that is used with.
 
     def __init__():
+
+        enteredLong = False
+        enteredShort = False
         pass
 
     def LongEnter(self):
+        if self.enteredShort or self.enteredLong:
+            return False
+        else:
+            pass
         pass
 
     def LongProfit(self):
-        pass
+        if not self.enteredLong:
+            return False
+        else:
+            pass
 
     def LongStopLoss(self):
-        pass
+        if not self.enteredLong:
+            return False
+        else:
+            pass
 
     def LongTimeout(self):
-        pass
+        if not self.enteredLong:
+            return False
+        else:
+            pass
 
     def ShortEnter(self):
-        pass
+        if self.enteredShort or self.enteredLong:
+            return False
+        else:
+            pass
 
     def ShortProfit(self):
-        pass
+        if not self.enteredShort:
+            return False
+        else:
+            pass
 
     def ShortStopLoss(self):
-        pass
+        if not self.enteredShort:
+            return False
+        else:
+            pass
 
     def ShortTimeout(self):
-        pass
+        if not self.enteredShort:
+            return False
+        else:
+            pass
 
     def Backtest(df):
         # This function accepts a df of historic stock data and an indicator logic object.
         # It will return a new dataframe of times when the indicator logic enters and exits long and short positions. 
-        # cols = ['Ticker', 'Date', 'Time', 'Action']
-        pass
+        
+        cols = ['Ticker', 'Date', 'Time', 'Action']
+        results = pd.DataFrame(columns=cols)
+
+        for row in df:
+            pass
+            
+
+
+        return results
