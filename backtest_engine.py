b00518c378440fe7957c314d673fb7967bd8cde2
kh3dron <tristansaldanha@gmail.com>
2024-01-25 16:56:34 -0800

    chip away

--- backtest_engine.py
+++ backtest_engine.py
@@ -4,10 +4,10 @@ class Backtest_Strategy():
     # This class should contain all the logic for a trading strategy to conduct a backtest. 
     # It will make assumptions about columns in the dataframe that is used with.
 
-    def __init__():
+    def __init__(self):
 
-        enteredLong = False
-        enteredShort = False
+        self.enteredLong = False
+        self.enteredShort = False
         pass
 
     def LongEnter(self):
@@ -59,7 +59,7 @@ class Backtest_Strategy():
         else:
             pass
 
-    def Backtest(df):
+    def Backtest(self, df):
         # This function accepts a df of historic stock data and an indicator logic object.
         # It will return a new dataframe of times when the indicator logic enters and exits long and short positions. 
         
@@ -67,8 +67,27 @@ class Backtest_Strategy():
         results = pd.DataFrame(columns=cols)
 
         for row in df:
-            pass
-            
+            action = None
+            if self.LongEnter(row):
+                action = "LongEnter"
+            elif self.LongProfit(row):
+                action = "LongProfit"
+            elif self.LongStopLoss(row):
+                action = "LongStopLoss"
+            elif self.LongTimeout():
+                action = "LongTimeout"
+            elif self.ShortEnter():
+                action = "ShortEnter"
+            elif self.ShortProfit():
+                action = "ShortProfit"
+            elif self.ShortStopLoss():
+                action = "ShortStopLoss"
+            elif self.ShortTimeout():
+                action = "ShortTimeout"
+            else:
+                pass
 
+            if action is not None: 
+                results.append({"Ticker": row["Ticker"], "Date": row["Date"], "Time": row["Time"], "Action": row["Action"]}, ignore_index=True)
 
         return results
